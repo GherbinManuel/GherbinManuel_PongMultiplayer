7c15da10f956dd4eb1d452f2ccfe27bc01080a0f
Manuel Gherbin <gherbin.manuel@gmail.com>
2025-07-28 21:35:12 +0200

    IP SHOWER

--- Assets/Content/Scripts/UI/UI_MainMenu.cs
+++ Assets/Content/Scripts/UI/UI_MainMenu.cs
@@ -1,15 +1,29 @@
 using Mirror;
+using System.Net.Sockets;
+using System.Net;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
 
 public class UI_MainMenu : UIBase
 {
     public override UIManager.UITypes UIType => UIManager.UITypes.MainMenu;
 
     [SerializeField] private TMP_InputField _ipInputField;
+    [SerializeField] private TextMeshProUGUI _localIPLabel;
     [SerializeField] private Button _hostButton;
     [SerializeField] private Button _joinButton;
+
+    public override void Init()
+    {
+        base.Init();
+        _localIPLabel.text = GetLocalIPv4();
+        StartCoroutine(GetPublicIP());
+
+        GUIUtility.systemCopyBuffer = _localIPLabel.text;
+    }
 
     public override void IsActive(bool isActive)
     {
@@ -32,5 +46,34 @@ public class UI_MainMenu : UIBase
         NetworkManager.singleton.networkAddress = _ipInputField.text;
         NetworkManager.singleton.StartClient();
 
+    }
+    private static string GetLocalIPv4()
+    {
+        string localIP = "";
+        foreach (var ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                localIP = ip.ToString();
+                break;
+            }
+        }
+        return localIP;
+    }
+
+    private IEnumerator GetPublicIP()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://api.ipify.org");
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log("IP pubblico: " + www.downloadHandler.text);
+            _localIPLabel.text = www.downloadHandler.text;
+        }
+        else
+        {
+            Debug.LogError("Errore nel recupero IP pubblico");
+        }
     }
 }
